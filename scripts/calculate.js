a5c40530f2f5ac484d7ace19ce89eec9486795a7
Откисян <77882873+otkisyan@users.noreply.github.com>
2022-05-18 10:40:48 +0200

    test

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -2,16 +2,15 @@
 //document.getElementById("sumButton").onclick = function() {finalSum()};
 
 
-function finalSum()
-{
-    
-    console.log("Sum button clicked");
-    var lastValue = document.getElementById("previousValue").valueAsNumber;
-    var newValue = document.getElementById("newValue").valueAsNumber;
-    var rate = document.getElementById("rate").valueAsNumber;
+function finalSum() {
 
-    var sum = newValue + rate + lastValue;
-    
-    document.getElementById("sum").valueAsNumber = sum.toFixed(2);
-    
+  console.log("Sum button clicked");
+  var lastValue = document.getElementById("previousValue").valueAsNumber;
+  var newValue = document.getElementById("newValue").valueAsNumber;
+  var rate = document.getElementById("rate").valueAsNumber;
+
+  var sum = (newValue - lastValue) * rate;
+
+  document.getElementById("sum").valueAsNumber = sum.toFixed(2);
+
 }
