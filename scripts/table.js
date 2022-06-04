3ce37a33b5d7ef7f3943e665fc8653f1c4902abd
Откисян <77882873+otkisyan@users.noreply.github.com>
2022-06-04 20:34:16 +0200

    entries archive features added

--- scripts/table.js
+++ scripts/table.js
@@ -20,6 +20,13 @@ function buildTable(){
     let Table = document.getElementById("tableData")
     let Entries = JSON.parse(localStorage.getItem('Entries' + type));
 
+    if (Entries[0].previousEntries === null){
+        document.getElementById("error").style.visibility = "visible"
+        document.getElementById("entriesTable").style.display = "none"
+        document.getElementById("errorImage").style.visibility = "visible"
+
+    }
+
     for(let i = 0; i < localStorage.getItem('EntriesSize' + type); i++){
         let row = `<tr>
             <td>${(Entries[i]).currentDate}</td>
